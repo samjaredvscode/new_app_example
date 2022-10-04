112a14111cfc7c47005007ba245f8fa42274cc55
Sam <samjared.02@gmail.com>
2022-10-04 18:59:29 -0500

    theme light

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Counter',
       home: const BodyApp(),
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
     );
   }
 }
