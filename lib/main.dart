b56492325dca08ca0305c5a9dbe69419225beac3
Sam <sam@pop-os.localdomain>
2022-10-04 18:57:26 -0500

    prueba de contador

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Contador',
+      title: 'Counter',
       home: const BodyApp(),
       theme: ThemeData.dark(),
     );
