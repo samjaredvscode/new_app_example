b460caa52a2ec7db544d0b13257f945316f5e14f
Sam <sam@pop-os.localdomain>
2022-10-04 18:50:59 -0500

    change name contador

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Counter',
+      title: 'Contador',
       home: const BodyApp(),
       theme: ThemeData.dark(),
     );
